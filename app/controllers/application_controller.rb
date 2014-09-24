9a41aa2d9c78a2c482b6db8059d44d8e564ddb34
Karl Freeman <karlfreeman@gmail.com>
2014-09-24 15:31:40 +0100

    locale, query params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_filter :set_locale
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || static_path(id: 'course')
   end
 
-  # private
+  private
 
-  # def set_locale
-  #   I18n.locale = current_user.locale if current_user
-  # end
+  def set_locale
+    desired_locale = I18n.default_locale
+    locale_subdomain = request.subdomain.empty? ? nil : request.subdomain.to_sym
+    if user_signed_in?
+      desired_locale = current_user.locale.to_sym
+      # the current_user has logged in, but is visiting an incorrect localised subdomain.
+      # lets point them in the right direction
+      if locale_subdomain && desired_locale != locale_subdomain
+        redirect_to(request.url.gsub(locale_subdomain.to_s, desired_locale.to_s))
+      end
+    elsif I18n.available_locales.include?(locale_subdomain)
+      desired_locale = locale_subdomain
+    elsif params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      desired_locale = params[:locale].to_sym
+    end
+
+    I18n.locale = desired_locale
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
-admin = AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-
-user = User.new(title: 'Dr', first_name: 'Demo', last_name: 'Account', email: 'demo@example.com', password: 'password', password_confirmation: 'password')
-user.skip_confirmation!
-user.save!
-
 chapter_one = Chapter.create!(title: 'Chapter 1')
 
 video_one = Video.create_for_chapter(chapter_one, title: 'Video 1')
 image_one = Image.create_for_chapter(chapter_one, title: 'Image 1')
 
-user.progressions.create!(step: video_one)
-user.progressions.create!(step: image_one)
+I18n.available_locales.each do |locale|
+  admin = AdminUser.create!(locale: locale, email: "#{locale}-admin@example.com", password: 'password', password_confirmation: 'password')
+  user = User.new(locale: locale, title: 'Ms', first_name: locale.capitalize, last_name: 'User', email: "#{locale}-user@example.com", password: 'password', password_confirmation: 'password')
+  user.skip_confirmation!
+  user.save!
+
+  user.progressions.create!(step: video_one)
+  user.progressions.create!(step: image_one)
+end
