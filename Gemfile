b9baf85f5e67e09ca6df3d58718d428c543113dc
Karl Freeman <karlfreeman@gmail.com>
2014-10-21 15:45:16 +0100

    pi

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ end
 group :development do
   gem 'bullet'
   gem 'quiet_assets'
+  gem 'rack-livereload'
+  gem 'guard-livereload', require: false
   gem 'binding_of_caller'
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+
+  #
+  config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
 end
