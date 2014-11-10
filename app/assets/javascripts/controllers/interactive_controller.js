8feaf069163855790d5466610039350d46a37bdc
Karl Freeman <karlfreeman@gmail.com>
2014-11-10 10:14:44 +0000

    scroller images

--- app/assets/javascripts/controllers/interactive_controller.js
+++ app/assets/javascripts/controllers/interactive_controller.js
@@ -6,5 +6,17 @@ Course.InteractivePageController = Ember.ObjectController.extend({
     next:function(chapter, step) {
       this.get('controllers.localeMenu').send('next', chapter, step);
     },
-  }
+  },
+  columns: function(){
+    switch(this.get('model.sources.length')) {
+      case 1:
+        return 'small-12 large-12';
+        break;
+      case 2:
+        return 'small-6 large-6';
+        break;
+      default:
+        return 'small-4 large-4';
+    }
+  }.property('model.sources.[]')
 });
--- app/assets/javascripts/models/interactive.js
+++ app/assets/javascripts/models/interactive.js
@@ -2,23 +2,18 @@ Course.Interactive = DS.Model.extend(Course.Page, {
   type: DS.attr('string', { defaultValue: 'Interactive' } ),
   body: DS.attr('string'),
   sources: DS.attr(),
+  imageHeight: 500,
+  steps: 10,
 
   sourcesWithUrls: function(){
-  var sources = this.get('sources') || Ember.A();
-   return sources.map(function(source) {
+    var sources = this.get('sources') || Ember.A();
+    return sources.map(function(source) {
      return '//djqy74tsvke0j.cloudfront.net/interactives/' + source + '.png';
-   });
+    });
   }.property('sources'),
-
-  triplet:function (){
-    return this.get('sources').get('length') == 3;
-  }.property('sources'),
-
-  imageHeight:function () {
-    return 500;
-  }.property('triplet'),
 
   imageHeightMax:function () {
-    return 5000;
-  }.property('triplet'),
+    return this.get('steps') * this.get('imageHeight');
+  }.property('steps', 'imageHeight'),
+
 });
