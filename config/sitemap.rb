b1643daf19808afaa1376d0a51cfdcc874f80aa0
Karl Freeman <karlfreeman@gmail.com>
2014-11-18 11:00:23 +0000

    sitemap

--- config/sitemap.rb
+++
@@ -1,6 +0,0 @@
-SitemapGenerator::Sitemap.default_host = 'https://www.readvizamyl.com'
-SitemapGenerator::Sitemap.create do
-  I18n.available_locales.each do |locale|
-    add "/#{locale}"
-  end
-end
