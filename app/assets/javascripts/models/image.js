f113364373ed5bd86d453b50a226d8c3f79b12e8
Karl Freeman <karlfreeman@gmail.com>
2014-11-20 17:13:59 +0000

    less pngs

--- app/assets/javascripts/models/image.js
+++ app/assets/javascripts/models/image.js
@@ -2,6 +2,6 @@ Course.Image = DS.Model.extend(Course.Page, {
   type: DS.attr('string', { defaultValue: 'Image' } ),
   source: DS.attr('string'),
   sourceWithUrl: function(){
-    return '//djqy74tsvke0j.cloudfront.net/images/' + this.get('source') + '.png';
+    return '//djqy74tsvke0j.cloudfront.net/images/' + this.get('source') + '.jpg';
   }.property('source'),
 });
