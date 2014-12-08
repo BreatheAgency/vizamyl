b67db00e4022e36042549f6f67ad1868f299770f
Karl Freeman <karlfreeman@gmail.com>
2014-12-08 16:03:24 +0000

    no longer needed

--- app/assets/javascripts/models/step.js
+++ app/assets/javascripts/models/step.js
@@ -14,14 +14,4 @@ Course.Step = DS.Model.extend({
     return this.get('progression.amount') === 1;
   }.property('progression.amount'),
 
-  progressClass: function(){
-    switch(this.get('progression.amount')) {
-      case 1:
-        return 'fi-checkbox';
-      case 0.5:
-        return 'fi-contrast';
-      default:
-        return 'fi-x-circle';
-    }
-  }.property('progression.amount'),
 });
