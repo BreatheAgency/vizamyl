9ef7dbfc8e5fad4b483525f7699beed87532344b
Karl Freeman <karlfreeman@gmail.com>
2014-12-10 11:34:36 +0000

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   end
 
   %w(404 422 500).each do |code|
-    match code, to: 'static#show', id: code
+    match code, to: 'static#show', id: code, via: [:get, :post]
   end
 
   get '/*id' => 'static#show', id: 'gatekeeper'
