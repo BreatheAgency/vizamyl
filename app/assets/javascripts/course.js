de163b8647a3f477e1a608ecbf49889eb58adc48
Karl Freeman <karlfreeman@gmail.com>
2014-12-11 11:13:41 +0000

    wip

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -3,3 +3,15 @@
 //= require handlebars
 //= require ember
 //= require ember-data
+//= require i18n
+//= require i18n/translations
+//= require ./ember-fsm
+//= require ./ember-google-analytics
+//= require_self
+//= require ./router
+//= require ./store
+
+window.Course = Ember.Application.create({
+  // LOG_TRANSITIONS: true,
+  // LOG_TRANSITIONS_INTERNAL: true,
+});
