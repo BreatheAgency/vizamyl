296a0a6263efd23c24afd994150f3acf6d4ff3c9
Karl Freeman <karlfreeman@gmail.com>
2014-12-23 14:26:46 -0500

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   def show
     clazz = params[:type].singularize.classify.constantize
     @page = clazz.includes(:translations).find(params[:id])
-    # @page = clazz.includes(:translations, question_rounds: [questions: [:translations, answers: [:translations]]]).find(params[:id])
     render json: @page
   end
 end
