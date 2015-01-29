9c976ad06f9928276b5c837372c7d29a42304c58
Karl Freeman <karlfreeman@gmail.com>
2015-01-29 15:04:15 +0000

    force

--- app/assets/javascripts/controllers/video_page_controller.js
+++ app/assets/javascripts/controllers/video_page_controller.js
@@ -4,6 +4,7 @@ Course.VideoPageController = Ember.ObjectController.extend({
   finished: Ember.computed.alias('controllers.application.currentStep.completed'),
 
   complete: function(){
+    return true;
     if (this.get('isSuperUser')) { return true; };
 
     if (this.get('mandatory')) {
