a91e0474c3725b0ca83c671fa98023f7ec459301
Karl Freeman <karlfreeman@gmail.com>
2015-02-02 09:20:57 +0000

    sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || "/course/#{I18n.locale}/#{current_user.latest_step.page_type.dasherize.downcase}/#{current_user.latest_step.page_id}"
+    request.env['omniauth.origin'] || stored_location_for(resource) || "/course/#{I18n.locale}/#{resource.latest_step.page_type.dasherize.downcase}/#{resource.latest_step.page_id}"
+  end
+
+  def after_sign_up_path_for(resource)
+    "/course/#{I18n.locale}/#{resource.latest_step.page_type.dasherize.downcase}/#{resource.latest_step.page_id}"
   end
 
   def set_admin_locale
