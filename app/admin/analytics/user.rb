3ac4ece7a0f8945ffd4954c277fe50c4cf9d9748
Karl Freeman <karlfreeman@gmail.com>
2015-02-04 11:36:18 +0000

    tweak admin interface

--- app/admin/analytics/user.rb
+++ app/admin/analytics/user.rb
@@ -10,14 +10,18 @@ ActiveAdmin.register User, as: 'Users' do
 
   index do
     selectable_column
-    column :id do |user|
-      link_to(user.id, admin_user_path(user))
+    column :full_name do |user|
+      link_to(user.full_name, admin_user_path(user))
     end
     column :email do |user|
       link_to(user.email, admin_user_path(user))
     end
-    column(:progress) {|model| "#{model.progress}%" }
-    column :final_assessment_status
+    column :progress do |user|
+      link_to("#{user.progress}%", admin_user_path(user))
+    end
+    column :final_assessment_status do |user|
+      link_to(user.final_assessment_status, admin_user_path(user))
+    end
     actions
   end
 
@@ -31,12 +35,11 @@ ActiveAdmin.register User, as: 'Users' do
 
   show title: :full_name do |user|
     attributes_table do
-      row :id
       row :full_name
       row :email
       row(:progress) {|model| "#{model.progress}%" }
-      row(:completed) { |model| model.completed? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
-      row(:passed) { |model| model.passed? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
+      # row(:completed) { |model| model.completed? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
+      # row(:passed) { |model| model.passed? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
       row :final_assessment_status
     end
   end
