cebf2f7ceee2038471750e047aa4569cb05d8ae6
Karl Freeman <karlfreeman@gmail.com>
2015-02-26 16:34:23 +0000

    specify locales

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,6 +7,9 @@ end
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  #
+  config.i18n.available_locales = [:en]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
