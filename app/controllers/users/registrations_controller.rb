13a36f363a39a9f06719585a588043bd3217a3ca
Karl Freeman <karlfreeman@gmail.com>
2015-03-04 15:52:14 +0000

    remove default sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,48 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters
-
-    def new
-     flash[:info] = 'Registrations are not open.'
-     redirect_to root_path
-   end
-
-   def create
-     flash[:info] = 'Registrations are not open.'
-     redirect_to root_path
-   end
-
-  def update
-    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
-
-    if account_update_params[:password].blank?
-      account_update_params.delete('password')
-      account_update_params.delete('password_confirmation')
-    end
-
-    # TODO
-    account_update_params.delete('password')
-    account_update_params.delete('password_confirmation')
-
-    @user = User.find(current_user.id)
-    if @user.update_attributes(account_update_params)
-      set_flash_message :notice, :updated
-      sign_in @user, bypass: true
-      respond_with @user, location: edit_user_registration_path
-    else
-      clean_up_passwords @user
-      respond_with @user
-    end
+  def new
+    redirect_to root_path
   end
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:salutation, :first_name, :last_name, :locale, :email, :password, :password_confirmation, :marketing_overall_opt_in, :marketing_email_opt_in, :marketing_post_opt_in, :marketing_representative_opt_in, :invite_code)
-    end
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:salutation, :first_name, :last_name, :password, :password_confirmation, :marketing_overall_opt_in, :marketing_email_opt_in, :marketing_post_opt_in, :marketing_representative_opt_in)
-    end
+  def create
+    redirect_to root_path
   end
-
 end
