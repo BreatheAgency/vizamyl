466014167c6f8366bf3a56fc10732d828bd1f078
Karl Freeman <karlfreeman@gmail.com>
2015-03-16 11:37:50 +0000

    wip

--- app/admin/analytics/dashboard.rb
+++ app/admin/analytics/dashboard.rb
@@ -1,2 +1,23 @@
 ActiveAdmin.register_page 'Dashboard' do
+  content do
+    panel 'Statistics' do
+      columns do
+        column do
+          render partial: 'users_total'
+        end
+        column do
+          render partial: 'users_total'
+        end
+        column do
+          render partial: 'users_total'
+        end
+        column do
+          render partial: 'users_total'
+        end
+      end
+    end
+    panel 'Visualisations' do
+      render partial: 'viz'
+    end
+  end
 end
