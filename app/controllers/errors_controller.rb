8ec45ab1be87ace1d9c4106fc50d661e0b9eb025
Karl Freeman <karlfreeman@gmail.com>
2015-04-01 11:59:55 +0100

    no need to check locales

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,7 @@
 class ErrorsController < ApplicationController
   layout 'gatekeeper'
+  skip_before_action :set_locale
+  skip_before_action :redirect_locale
 
   def file_not_found
   end
--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,6 +1,7 @@
 class RobotsController < ApplicationController
   layout nil
   skip_before_action :set_locale
+  skip_before_action :redirect_locale
 
   def index
     headers['Content-Type'] = 'text/plain'
