4051c5eb6780e8c03441b03dd83e3cb53d115525
Karl Freeman <karlfreeman@gmail.com>
2015-04-24 11:49:34 +0100

    remove black triangle logic

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :locale, :super_user, :black_triangle_viewed_at, :failed_round_one_at, :failed_round_two_at, :passed_round_one_at, :passed_round_two_at, :completed
+  attributes :id, :email, :locale, :super_user, :failed_round_one_at, :failed_round_two_at, :passed_round_one_at, :passed_round_two_at, :completed
   has_many :progressions
   has_many :steps
 
