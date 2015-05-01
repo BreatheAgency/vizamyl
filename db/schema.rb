1f109eeae1088d879538348dba82eb43b50fbc48
Karl Freeman <karlfreeman@gmail.com>
2015-05-01 13:38:37 +0100

    cookies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428153132) do
+ActiveRecord::Schema.define(version: 20150501110635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -329,6 +329,7 @@ ActiveRecord::Schema.define(version: 20150428153132) do
     t.boolean  "marketing_representative_opt_in",             default: false
     t.text     "institution",                                 default: ""
     t.string   "invite_code"
+    t.boolean  "cookies_opt_in",                              default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
