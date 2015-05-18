fcf6a0fa3cf23e28ebc67e6893891e590aeab32d
Karl Freeman <karlfreeman@gmail.com>
2015-05-18 11:36:09 +0100

    migrate

--- migrate_locale.sql
+++ migrate_locale.sql
@@ -1,76 +1,76 @@
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM answer_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM answer_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('answer_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO answer_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM chapter_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM chapter_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('chapter_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO chapter_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM image_source_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM image_source_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('image_source_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO image_source_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM image_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM image_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('image_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO image_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM interactive_source_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM interactive_source_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('interactive_source_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO interactive_source_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM interactive_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM interactive_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('interactive_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO interactive_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM question_intro_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM question_intro_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('question_intro_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO question_intro_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM question_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM question_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('question_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO question_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM test_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM test_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('test_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO test_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM text_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM text_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('text_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO text_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
 
 DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM video_translations;
+CREATE TEMPORARY TABLE tmptable as SELECT * FROM video_translations WHERE locale = 'en';
 UPDATE tmptable SET id = nextval('video_translations_id_seq');
-UPDATE tmptable SET locale = 'de';
+UPDATE tmptable SET locale = 'fr';
 INSERT INTO video_translations (SELECT * FROM tmptable);
 DROP TABLE IF EXISTS tmptable;
--- replace_asset.sql
+++ replace_asset.sql
@@ -1,10 +1,7 @@
-DROP TABLE IF EXISTS tmptable;
-CREATE TEMPORARY TABLE tmptable as SELECT * FROM image_source_translations;
-UPDATE tmptable SET id = nextval('image_source_translations_id_seq');¬
-UPDATE tmptable SET locale = 'de';
-INSERT INTO image_source_translations (SELECT * FROM tmptable);¬
-DROP TABLE IF EXISTS tmptable;
-
 UPDATE video_translations
-SET source = replace(source, 'EN_','DE_')
-WHERE locale='de'
+SET source = replace(source, 'EN_','FR_')
+WHERE locale='fr';
+
+UPDATE image_source_translations
+SET source = replace(source, 'EN_','FR_')
+WHERE locale='fr';
