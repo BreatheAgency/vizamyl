1271bf39c0638a70567958923ce35b155bb9b0a8
Karl Freeman <karlfreeman@gmail.com>
2015-05-20 11:56:04 +0100

    tidy up

--- scratchpad.rb
+++ scratchpad.rb
@@ -9,3 +9,10 @@ qs.each { |n| n.update_attributes(correct_title: 'Question ${index}: Correct', i
 I18n.locale = :de
 t.each { |n| n.update_attribute(:results_title, 'Ergebnis der Fragen zur Selbstbewertung') }
 qs.each { |n| n.update_attributes(correct_title: 'Frage ${index}: Richtig', incorrect_title: 'Frage ${index}: Falsch') }
+
+
+# User.new(locale: 'fr', invite_code: 'resonance', salutation: 'Dr', first_name: 'Example', last_name: 'Example', email: 'fr-super-user@readvizamyl.com', password: 'w9aMyMm2Fd', password_confirmation: 'w9aMyMm2Fd', institution: 'Example', super_user: true, terms_and_conditions_opt_in: true).save!
+
+# AdminUser.new(locale: 'fr', email: 'fr-admin@readvizamyl.com', password: 'jGsHZes5dCeWR6a', password_confirmation: 'jGsHZes5dCeWR6a').save!
+
+# AdminUser.new(locale: 'fr', email: 'fr-content-admin@readvizamyl.com', password: 'si8Enf939Q9I6mQ', password_confirmation: 'si8Enf939Q9I6mQ').save!
