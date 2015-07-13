f80afd0ee39b382859ee7f4061a71063784cc6fe
Karl Freeman <karlfreeman@gmail.com>
2015-07-13 16:02:42 +0100

    wip

--- scratchpad.rb
+++ scratchpad.rb
@@ -11,8 +11,8 @@ t.each { |n| n.update_attribute(:results_title, 'Ergebnis der Fragen zur Selbstb
 qs.each { |n| n.update_attributes(correct_title: 'Frage ${index}: Richtig', incorrect_title: 'Frage ${index}: Falsch') }
 
 
-User.new(locale: 'it', invite_code: 'plaque', salutation: 'Dr', first_name: 'Example', last_name: 'Example', email: 'it-super-user@readvizamyl.com', password: 'Ko94Pj1Yd4tXh2i', password_confirmation: 'Ko94Pj1Yd4tXh2i', institution: 'Example', super_user: true, terms_and_conditions_opt_in: true).save!
+User.new(locale: 'en-gb', invite_code: 'plaque', salutation: 'Dr', first_name: 'Example', last_name: 'Example', email: 'en-gb-super-user@readvizamyl.com', password: 'InjeiPLiN4HCQaO', password_confirmation: 'InjeiPLiN4HCQaO', institution: 'Example', super_user: true, terms_and_conditions_opt_in: true).save!
 
-AdminUser.new(locale: 'it', email: 'it-admin@readvizamyl.com', password: 'phS4NT12JtGWAnY', password_confirmation: 'phS4NT12JtGWAnY').save!
+AdminUser.new(locale: 'en-gb', email: 'en-gb-admin@readvizamyl.com', password: 'q8q6ZhLvEfJ3gsm', password_confirmation: 'q8q6ZhLvEfJ3gsm').save!
 
-AdminUser.new(locale: 'it', email: 'it-content-admin@readvizamyl.com', password: 'ZCa2gZIC883OJXQ', password_confirmation: 'ZCa2gZIC883OJXQ').save!
+AdminUser.new(locale: 'en-gb', email: 'en-gb-content-admin@readvizamyl.com', password: 'jxC5vWXbzx15V9X', password_confirmation: 'jxC5vWXbzx15V9X').save!
