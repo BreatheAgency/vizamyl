cdae8fbc883ed692de6c89e6e5c1d891399f8788
Karl Freeman <karlfreeman@gmail.com>
2015-09-03 16:33:58 +0100

    fix

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -1,6 +1,7 @@
 Course.ApplicationController = Ember.Controller.extend({
-  environment: "<%= Rails.env %>",
-  isSandbox: Ember.computed.equal('environment', 'sandbox'),
+  isDevelopment: Ember.computed.equal('currentEnvironment', 'development'),
+  isSandbox: Ember.computed.equal('currentEnvironment', 'sandbox'),
+  isProduction: Ember.computed.equal('currentEnvironment', 'production'),
   currentLocale: null,
   currentPage: null,
   signedIn: Ember.computed.notEmpty('currentUser'),
@@ -18,5 +19,9 @@ Course.ApplicationController = Ember.Controller.extend({
   currentUser: function() {
     return Course.get('user');
   }.property('Course.user'),
+
+  currentEnvironment: function() {
+    return Course.get('env.environment')
+  }.property('Course.env.environment')
 
 });
