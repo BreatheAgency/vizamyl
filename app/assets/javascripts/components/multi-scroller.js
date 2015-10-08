4740db5d248ab5c4c7a8193878de71ef18a393c7
Karl Freeman <karlfreeman@gmail.com>
2015-10-08 14:06:35 +0100

    wip

---
+++ app/assets/javascripts/components/multi-scroller.js
@@ -0,0 +1,61 @@
+Course.MultiScrollerComponent = Ember.Component.extend({
+  src: '',
+  loaded: false,
+  imageStep: 0,
+  imageWidth: 0,
+  imageHeight: 500,
+  imageHeightMax: 0,
+  images: [],
+  imagesLoaded: 0,
+
+  didInsertElement: function() {
+    var that = this;
+    var drag = new Dragdealer(this.$('.dragdealer').get(0), {
+      disabled: true,
+      horizontal: false,
+      vertical: true,
+      snap: true,
+      y: that.get('imageStep'),
+      steps: that.get('imageStepMax'),
+      animationCallback: function(x, y) {
+        if (!that.get('drag')) { return; } // in case `drag` isn't yet initialized
+        that.set('imageStep', that.get('drag').getStep()[1] - 1);
+      },
+    });
+    this.set('drag', drag);
+    this.get('drag').enable();
+    this.get('images').forEach(function(image) {
+      console.log(image);
+      this.$('<img>').attr('src', image).one('load', function() {
+        this.incrementProperty('imagesLoaded');
+        // console.log(this.get('imagesLoaded') + " | " + this.get('images.length'));
+      }.bind(that));
+    });
+    // this.$('<img>').attr('src', this.get('source')).one('load', function() {
+    //   this.incrementProperty('imagesLoaded');
+    //   console.log(this.get('imagesLoaded') + " | " + this.get('images.length'));
+    //   this.set('loaded', true);
+    //   var image_holder = this.$('.image_holder').detach();
+    //   this.$('.image_wrapper').empty().append(image_holder);
+    //   this.$('.image_holder').css('background-image', "url(" + this.get('source') + ")");
+    // }.bind(this));
+  }.on('didInsertElement'),
+
+  imageStepMax: function() {
+    return Math.round(this.get('imageHeightMax') / this.get('imageHeight'));
+  }.property('imageHeight', 'imageHeightMax'),
+
+  style: function() {
+    return "background-color: black; height:" + this.get('imageHeight') + "px;width:" + this.get('imageWidth') + "px;background-size:" + this.get('imageWidth') + "px " + this.get('imageHeightMax') + "px;" + "background-position:" + "0px 0px;";
+  }.property('imageHeight', 'imageHeightMax', 'imageWidth', 'source'),
+
+  onScroll: function() {
+    var currentHeight;
+    Ember.run.scheduleOnce('afterRender', this, function(){
+      currentHeight = Math.round((this.get('imageStep')) * this.get('imageHeight'));
+      this.$('.image_holder').css({
+        "background-position": "0px " + parseInt(this.get('imageHeightMax') - currentHeight) + "px"
+      })
+    });
+  }.observes('imageStep'),
+});
