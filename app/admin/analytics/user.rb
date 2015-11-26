25ce807d4501dac616528d4aad48f62c24d40326
Karl Freeman <karlfreeman@gmail.com>
2015-11-26 14:09:17 +0000

    csv

--- app/admin/analytics/user.rb
+++ app/admin/analytics/user.rb
@@ -1,18 +1,23 @@
 ActiveAdmin.register User, as: 'Users' do
   actions :all, except: [:new, :destroy, :edit]
-  config.filters = false
+  filter :locale, as: :select, collection: proc { I18n.available_locales }
+  filter :origin, as: :select, collection: proc { IsoCountryCodes::Code.for_select }
+  filter :institution, as: :string
 
   controller do
     def permitted_params
       params.permit!
     end
-    def scoped_collection
-      super.where(locale: I18n.locale)
-    end
   end
 
   index do
     selectable_column
+    column :locale do |user|
+      link_to(user.locale, admin_user_path(user))
+    end
+    column :origin do |user|
+      link_to(user.origin, admin_user_path(user))
+    end
     column :full_name do |user|
       link_to(user.full_name, admin_user_path(user))
     end
@@ -48,19 +53,36 @@ ActiveAdmin.register User, as: 'Users' do
 
   show title: :full_name do |user|
     attributes_table do
+      row :locale
+      row :origin
       row :full_name
       row :email
       row :institution
       row(:progress) {|model| "#{model.progress}%" }
-      # row(:completed) { |model| model.completed? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
-      # row(:passed) { |model| model.passed? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
       row :final_assessment_status
       row :invite_code
-      row('Cookies') { |model| model.cookies_opt_in? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
       row('Overall Marketing') { |model| model.marketing_overall_opt_in? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
       row('Email Marketing') { |model| model.marketing_email_opt_in? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
       row('Post Marketing') { |model| model.marketing_post_opt_in? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
       row('Representative Marketing') { |model| model.marketing_representative_opt_in? ? status_tag( 'yes', :ok )  : status_tag( 'no', :ok ) }
     end
+  end
+
+  csv do
+    column :created_at
+    column :locale
+    column :origin
+    column :salutation
+    column :first_name
+    column :last_name
+    column :email
+    column :institution
+    column :invite_code
+    column :passed
+    column :progress
+    column :marketing_overall_opt_in
+    column :marketing_email_opt_in
+    column :marketing_post_opt_in
+    column :marketing_representative_opt_in
   end
 end
