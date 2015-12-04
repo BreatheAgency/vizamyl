27a728c7a04d87c344094b77a3623ca441909bc9
Karl Freeman <karlfreeman@gmail.com>
2015-12-04 12:29:54 +0000

    fix origins

--- db/migrate/20151125110016_add_origin_to_users.rb
+++ db/migrate/20151125110016_add_origin_to_users.rb
@@ -1,7 +1,9 @@
 class AddOriginToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :origin, :string, default: 'EN', null: false
-    User.update_all('locale=origin')
+    User.update_all('origin=locale')
+    User.where(origin: 'en-gb').update_all(origin: 'gb')
+    User.where(origin: 'de-at').update_all(origin: 'at')
   end
 
   def self.down
