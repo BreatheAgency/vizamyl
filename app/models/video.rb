99afcbfe2300b6fbc8ffc54dd1f3fc86d1fbac87
Karl Freeman <karlfreeman@gmail.com>
2016-05-06 10:10:49 +0100

    US optional

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,4 +6,17 @@ class Video < ActiveRecord::Base
     translates translated_field
   end
 
+  # FIXME
+  # For certain US videos, they would like mandatory videos to be optional
+  # Globalize doenst support localised booleans so this is a placeholder
+  # til I find a better solution
+  US_OPTIONAL_VIDEO_IDS = [1]
+  def mandatory
+    if (I18n.locale == :"en-us" && US_OPTIONAL_VIDEO_IDS.include?(self.id))
+      false
+    else
+      read_attribute(:mandatory)
+    end
+  end
+
 end
