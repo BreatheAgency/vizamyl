e8f6cbfb0205cb3fa326080e914ea1db0ac03729
Karl Freeman <karlfreeman@gmail.com>
2016-05-12 10:51:58 +0100

    wip

--- app/controllers/users/course_completion_controller.rb
+++ app/controllers/users/course_completion_controller.rb
@@ -17,8 +17,7 @@ class CourseCompletionLetter < Prawn::Document
     text_box(I18n.t('course_completion.salutation') + ': ' + user.salutation, at: [left, top + (margin*4) + (height*4)], style: :normal, size: 14)
     text_box(I18n.t('course_completion.first_name') + ': ' + user.first_name, at: [left, top + (margin*3) + (height*3)], style: :normal, size: 14)
     text_box(I18n.t('course_completion.last_name') + ': ' + user.last_name, at: [left, top + (margin*2) + (height*2)], style: :normal, size: 14)
-    date_format = "%m/%d/%Y"
-    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(Time.now, locale: user.locale, format: date_format), at: [left, top + margin + height], style: :normal, size: 14)
+    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(Time.now, locale: user.locale, format: "%m/%d/%Y"), at: [left, top + margin + height], style: :normal, size: 14)
   end
 end
 
