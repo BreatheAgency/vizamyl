7509f94f8de4cce9dc80bde7287fd51200c86f10
Karl Freeman <karlfreeman@gmail.com>
2016-05-25 16:26:24 +0100

    wip

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
   def new
     redirect_to root_path
   end
@@ -6,4 +8,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     redirect_to root_path
   end
+
+  protected
+
+  def after_update_path_for(resource)
+    edit_user_registration_path
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :salutation
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :last_name
+  end
+
 end
