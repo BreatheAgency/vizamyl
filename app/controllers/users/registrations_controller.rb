971596fca06cc50c969102d9994018b134e4d113
Karl Freeman <karlfreeman@gmail.com>
2016-05-25 17:09:15 +0100

    wip

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,13 +16,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    resource.update_attributes(params)
   end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :salutation
     devise_parameter_sanitizer.for(:account_update) << :first_name
     devise_parameter_sanitizer.for(:account_update) << :last_name
+    devise_parameter_sanitizer.for(:account_update) << :password
+    devise_parameter_sanitizer.for(:account_update) << :password_confirmation
   end
 
 end
