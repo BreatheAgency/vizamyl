9f3a5d02e6c119d6e39d9c836cbd5a35fd2087ce
Andrew Nagi <andrew.nagi@gmail.com>
2023-11-06 07:18:21 +0700

    Add js to open / close nenu

--- app/assets/javascripts/libs.js
+++ app/assets/javascripts/libs.js
@@ -1,1 +1,14 @@
 //= require jquery
+
+$(document).ready(function () {
+  $(".header__bar , .offcanvas-overlay").click(function () {
+    if($(".offcanvas-area").hasClass('active')) {
+      $(".offcanvas-area , .offcanvas-overlay").removeClass("active");
+    } else {
+      $(".offcanvas-area , .offcanvas-overlay").addClass("active");
+    }
+  });
+  $(".menu-close , .offcanvas-overlay").click(function () {
+    $(".offcanvas-area , .offcanvas-overlay").removeClass("active");
+  });
+});
