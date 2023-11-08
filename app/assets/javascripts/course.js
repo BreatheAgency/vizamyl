de2af4b38f834fb33215e0fa52e617eed6da5bfe
Andrew Nagi <andrew.nagi@gmail.com>
2023-11-08 16:21:05 +0700

    Course header and footer

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -55,3 +55,16 @@ Ember.RSVP.on('error', function (error) {
     window.location = '/';
   }
 });
+
+$(document).ready(function () {
+  $(".header__bar , .offcanvas-overlay").click(function () {
+    if($(".offcanvas-area").hasClass('active')) {
+      $(".offcanvas-area , .offcanvas-overlay").removeClass("active");
+    } else {
+      $(".offcanvas-area , .offcanvas-overlay").addClass("active");
+    }
+  });
+  $(".menu-close , .offcanvas-overlay").click(function () {
+    $(".offcanvas-area , .offcanvas-overlay").removeClass("active");
+  });
+});
