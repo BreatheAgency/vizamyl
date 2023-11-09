e64d95769f571ed8806307c8eb9cc6809ef1f3a7
Andrew Nagi <andrew.nagi@gmail.com>
2023-11-09 13:40:31 +0700

    Make progressions responsive

--- app/assets/javascripts/components/visual-progression.js
+++ app/assets/javascripts/components/visual-progression.js
@@ -2,8 +2,4 @@ Course.VisualProgressionComponent = Ember.Component.extend({
   classNames: ['visual-progression'],
   tagName: 'ul',
   total: 0,
-
-  totalPercent:function(){
-    return this.get('total') + '%';
-  }.property('total')
 });
