5fd23d865033109af2c82f5cbb65a73f08c9d587
Andrew Nagi <andrew.nagi@gmail.com>
2023-12-23 14:25:37 +0000

    Fix interactives

--- app/assets/javascripts/models/interactive_source.js
+++ app/assets/javascripts/models/interactive_source.js
@@ -2,12 +2,18 @@ Course.InteractiveSource = DS.Model.extend({
   label: DS.attr('string'),
   source: DS.attr('string'),
   height: DS.attr('number'),
-  hasUnsafeHeight: Ember.computed.gte('height', 20000),
+  hasUnsafeHeight: Ember.computed.gte('height', 20000), // At the time of writing, 151 out of 161 meet this condition
 
+  // Called if hasUnafeHeight == false
+  // This was the way it was originally done. Load one tall image with all frames included.
+  // Have a look on S3 at one of the images to see what that means.
   sourceWithUrl:function() {
     return '//' + Course.get('env.content-host') + '/sliders/single/' + this.get('source') + '.jpg';
   }.property('source'),
 
+  // Called if hasUnafeHeight == true
+  // This looks like it was a bugfix for very tall images. The big image got broken into separate images 500px
+  // tall each.
   images:function () {
     var amount = this.get('height') / 500;
     var images = [];
