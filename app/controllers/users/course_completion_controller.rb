ead2aa5402a104b73a0f3cbd1ae09f1e01d6bbaa
Waqas <waqas@multivitamin.studio>
2024-05-13 14:33:19 +0100

    Add Course Completion Certificate Functionality

--- app/controllers/users/course_completion_controller.rb
+++ app/controllers/users/course_completion_controller.rb
@@ -1,5 +1,5 @@
 class CourseCompletionLetter < Prawn::Document
-  def initialize(user)
+  def initialize(user, from_active_admin = false)
     super(page_size: 'LETTER', page_layout: :portrait, left_margin: 0, right_margin: 0, top_margin: 0, bottom_margin: 0)
     font_families.update({
       'geinspira' => {
@@ -17,12 +17,14 @@ class CourseCompletionLetter < Prawn::Document
     text_box(I18n.t('course_completion.salutation') + ': ' + user.salutation, at: [left, top + (margin*4) + (height*4)], style: :normal, size: 14) if user.salutation.present?
     text_box(I18n.t('course_completion.first_name') + ': ' + user.first_name, at: [left, top + (margin*3) + (height*3)], style: :normal, size: 14)
     text_box(I18n.t('course_completion.last_name') + ': ' + user.last_name, at: [left, top + (margin*2) + (height*2)], style: :normal, size: 14)
-    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(Time.now, locale: user.locale, format: "%m/%d/%Y"), at: [left, top + margin + height], style: :normal, size: 14)
+    completion_date = from_active_admin ? user.passed_round_two_at : Time.now
+    completion_date ||= Time.now # Fallback to Time.now if user.passed_round_two_at is nil
+    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(completion_date, locale: user.locale, format: "%m/%d/%Y"), at: [left, top + margin + height], style: :normal, size: 14)
   end
 end
 
 class CourseCompletionA4 < Prawn::Document
-  def initialize(user)
+  def initialize(user, from_active_admin = false)
     super(page_size: 'A4', page_layout: :portrait, left_margin: 0, right_margin: 0, top_margin: 0, bottom_margin: 0)
     font_families.update({
       'geinspira' => {
@@ -40,19 +42,21 @@ class CourseCompletionA4 < Prawn::Document
     text_box(I18n.t('course_completion.salutation') + ': ' + user.salutation, at: [left, top + (margin*4) + (height*4)], style: :normal, size: 14) if user.salutation.present?
     text_box(I18n.t('course_completion.first_name') + ': ' + user.first_name, at: [left, top + (margin*3) + (height*3)], style: :normal, size: 14)
     text_box(I18n.t('course_completion.last_name') + ': ' + user.last_name, at: [left, top + (margin*2) + (height*2)], style: :normal, size: 14)
+    completion_date = from_active_admin ? user.passed_round_two_at : Time.now
+    completion_date ||= Time.now # Fallback to Time.now if user.passed_round_two_at is nil
     date_format = (user.locale == 'en-us') ? "%m/%d/%Y" : "%d/%m/%Y"
-    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(Time.now, locale: user.locale, format: date_format), at: [left, top + margin + height], style: :normal, size: 14)
+    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(completion_date, locale: user.locale, format: date_format), at: [left, top + margin + height], style: :normal, size: 14)
   end
 end
 
 class CourseCompletionA4WithDepartment < Prawn::Document
-  def initialize(user)
+  def initialize(user, from_active_admin = false)
     super(page_size: 'A4', page_layout: :portrait, left_margin: 0, right_margin: 0, top_margin: 0, bottom_margin: 0)
     font_families.update({
-                           'arial-ms' => {
-                             normal: Rails.root.join('app/assets/fonts/ArialUnicodeMS.ttf'),
-                           }
-                         })
+      'arial-ms' => {
+        normal: Rails.root.join('app/assets/fonts/ArialUnicodeMS.ttf'),
+      }
+    })
     font('arial-ms')
     image(Rails.root.join("app/assets/images/course-completion-#{I18n.locale}.jpg"), at: [bounds.absolute_left, PDF::Core::PageGeometry::SIZES['A4'][1] - bounds.absolute_bottom], fit: PDF::Core::PageGeometry::SIZES['A4'])
     left = 38
@@ -64,23 +68,28 @@ class CourseCompletionA4WithDepartment < Prawn::Document
     text_box(I18n.t('course_completion.department') + ': ' + user.department.to_s, at: [left, top + (margin*4) + (height*4)], style: :normal, size: 14)
     text_box(I18n.t('course_completion.last_name') + ': ' + user.last_name, at: [left, top + (margin*3) + (height*3)], style: :normal, size: 14)
     text_box(I18n.t('course_completion.first_name') + ': ' + user.first_name, at: [left, top + (margin*2) + (height*2)], style: :normal, size: 14)
+    completion_date = from_active_admin ? user.passed_round_two_at : Time.now
+    completion_date ||= Time.now # Fallback to Time.now if user.passed_round_two_at is nil
     date_format = (user.locale == 'en-us') ? "%m/%d/%Y" : "%d/%m/%Y"
-    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(Time.now, locale: user.locale, format: date_format), at: [left, top + margin + height], style: :normal, size: 14)
+    text_box(I18n.t('course_completion.date') + ': ' + I18n.l(completion_date, locale: user.locale, format: date_format), at: [left, top + margin + height], style: :normal, size: 14)
   end
 end
 
 class Users::CourseCompletionController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :skip_authentication_for_success
 
   def success
-    current_user.pass!
+    user = current_user || User.find(params[:user_id])
+    user.pass! if current_user
+
+    from_active_admin = params[:from_active_admin] == 'true' if params[:from_active_admin]
 
     if european_locale?
-      pdf = CourseCompletionA4.new(current_user)
+      pdf = CourseCompletionA4.new(user, from_active_admin)
     elsif japanese_locale?
-      pdf = CourseCompletionA4WithDepartment.new(current_user)
+      pdf = CourseCompletionA4WithDepartment.new(user, from_active_admin)
     else
-      pdf = CourseCompletionLetter.new(current_user)
+      pdf = CourseCompletionLetter.new(user, from_active_admin)
     end
 
     respond_to do |format|
@@ -91,9 +100,7 @@ class Users::CourseCompletionController < ApplicationController
     end
   end
 
-  def adjunctive_use_of_quantification
-  end
-
-  def refresher_video
+  def skip_authentication_for_success
+    params[:action] == 'success'
   end
 end
