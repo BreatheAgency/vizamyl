1675ab05affaa16f1192262e136311755669d72a
Shaqiel Heradien <shaqielheradien@gmail.com>
2026-02-25 22:19:19 +0200

    Add locale redirects to live readvizamyl.com for dev demo

---
+++ config/initializers/dev_locale_redirects.rb
@@ -0,0 +1,13 @@
+# Redirect locale paths to live readvizamyl.com (dev only)
+# Excludes French (/fr)
+if ENV['REDIRECT_LOCALES_TO_LIVE'] == 'true'
+  Rails.application.config.middleware.insert_before 0, Rack::Rewrite do
+    %w[jp en en-us en-gb de-at de it es].each do |locale|
+      r301 %r{^/#{Regexp.escape(locale)}(/.*)?(\?.*)?$}, lambda { |match, rack_env|
+        path = match[1] || ''
+        query = rack_env['QUERY_STRING'].present? ? "?#{rack_env['QUERY_STRING']}" : ''
+        "https://#{locale}.readvizamyl.com/#{locale}#{path}#{query}"
+      }
+    end
+  end
+end
