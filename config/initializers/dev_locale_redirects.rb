0df0b5d360293592ba78e07c852aac22c14acf23
Shaqiel Heradien <shaqielheradien@gmail.com>
2026-02-25 22:46:57 +0200

    Fix locale redirects - exclude en-gb and fr

--- config/initializers/dev_locale_redirects.rb
+++ config/initializers/dev_locale_redirects.rb
@@ -1,8 +1,8 @@
-%w[jp en en-us de-at de it es].each do |locale|X# Redirect locale paths to live readvizamyl.com (dev only)
-# Excludes French (/fr)
+# Redirect locale paths to live readvizamyl.com (dev only)
+# Excludes French (/fr) and UK English (/en-gb)
 if ENV['REDIRECT_LOCALES_TO_LIVE'] == 'true'
   Rails.application.config.middleware.insert_before 0, Rack::Rewrite do
-    %w[jp en en-us en-gb de-at de it es].each do |locale|
+    %w[jp en en-us de-at de it es].each do |locale|
       r301 %r{^/#{Regexp.escape(locale)}(/.*)?(\?.*)?$}, lambda { |match, rack_env|
         path = match[1] || ''
         query = rack_env['QUERY_STRING'].present? ? "?#{rack_env['QUERY_STRING']}" : ''
