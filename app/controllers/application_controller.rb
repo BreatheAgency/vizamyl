68fa004ecc255ec5637eaa882c195ec2e92d282f
Archie Makuwa <archie.makuwa@brafton.com>
2026-07-15 18:55:01 +0200

    resolvd cross-subdomain login failure and session-sharing loops - update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_origin
-  before_action :redirect_locale
+  before_action :set_locale
 
   # Dynamic default url options. Ensures absolute links are compiled against
   # whichever subdomain/domain the user is currently interacting with.
