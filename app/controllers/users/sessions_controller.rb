0a78bd9ddac6069bf2511cfecf5512ecb4611e59
Archie Makuwa <archie.makuwa@brafton.com>
2026-08-25 15:45:34 +0200

    Updated session controller to keep the same footer after login -1

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,12 @@
+# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
-  before_action :reject_admin, except: :destroy
+  def after_sign_in_path_for(resource)
+    locale = params[:locale].presence || I18n.locale
+    "/#{locale}/home"
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    locale = params[:locale].presence || I18n.locale
+    "/#{locale}/home"
+  end
 end
